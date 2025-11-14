5e66089c67f3c55ef81be56e70c11184e9e86a38
ido nitay <idonitay.eini@gmail.com>
2025-11-14 11:28:26 +0200

    Add html Page

--- JavaScript/Canvas.js
+++ JavaScript/Canvas.js
@@ -1,3 +1,4 @@
+//create a class that is the canvas - where you can draw - it is a matrix, that when a player draws, the matrix changes and the screen canvas updates acoordingly
 class Canvas {
     constructor(width, height, x0, y0)
     {
@@ -8,11 +9,13 @@ class Canvas {
         this.mat = this.createMatrix(this.width, this.height, "#FFFFFF")
     }
 
+    //create a matrix with a given value
     createMatrix(rows, cols, defaultValue = 0) 
     {
         return Array(rows).fill().map(() => Array(cols).fill(defaultValue));
     }
 
+    //change the value of a color of a single cell
     changeCellColor(row, col, color)
     {
         if (row < this.x0 + this.width && col < this.y0 + this.height && rox > this.x0 && col > this.y0)
