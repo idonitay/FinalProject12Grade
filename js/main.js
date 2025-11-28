5f29a0112b30eb43a037622b2912daf9926f181a
ido nitay <idonitay.eini@gmail.com>
2025-11-28 12:25:11 +0200

    Player can draw - but not stop - WIP

--- js/main.js
+++ js/main.js
@@ -45,24 +45,71 @@ function createCanvas(id, parent, styles) {
     return canvas;
 }
 
-function drawLine(canvas, p1, p2)
-{
-    //finding the canvas
-    
+function drawLine(canvasOrId, p1, p2) {
+    // Find the canvas element
+    const canvas =
+        typeof canvasOrId === "string"
+            ? document.getElementById(canvasOrId)
+            : canvasOrId;
+
     const ctx = canvas.getContext("2d");
-    
-    //styling the brush
+    //stylethe brush
     ctx.lineWidth = 2;
     ctx.strokeStyle = "black";
-    
-    //drawing the line
+
+    //draw the line
     ctx.beginPath();
     ctx.moveTo(p1.x, p1.y);
     ctx.lineTo(p2.x, p2.y);
     ctx.stroke();
 }
 
+
+function playerDrawHandler() {
+    let points = [];
+
+    canvas.onclick = function (e) {
+        const rect = canvas.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        points.push({ x, y });
+
+        canvas.onmousemove = function (moveEvent) {
+            const rect = canvas.getBoundingClientRect();
+            const x = moveEvent.clientX - rect.left;
+            const y = moveEvent.clientY - rect.top;
+
+            points.push({ x, y });
+
+            drawLine(
+                "canvas",
+                points[points.length - 2],
+                points[points.length - 1]
+            );
+
+            canvas.mouseup = function () 
+            {
+                points = [];
+                canvas.onmousemove = null;
+            };
+        };
+    };
+
+    canvas.mouseup = function () 
+    {
+        points = [];
+        canvas.onmousemove = null;
+    };
+
+    
+}
+
+
 canvas_wrapper_div = createDiv("canvas-wrapper-div", body_div, ["canvas_wrapper_div"])
 canvas = createCanvas("canvas", canvas_wrapper_div, []);
+canvas.addEventListener('click', playerDrawHandler)
 
-drawLine(canvas=canvas, p1={x: 4, y: 8}, p2={x: 200, y: 200})
+// drawLine(canvas=canvas, p1={x: 40, y: 80}, p2={x: 200, y: 200})
+// drawLine(canvas=canvas, p1={x: 70, y: 68}, p2={x: 300, y: 100})
+
