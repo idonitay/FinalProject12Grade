d18b51736768fa60265785c40e35c3750b761a58
ido nitay <idonitay.eini@gmail.com>
2025-11-28 10:58:18 +0200

    Drawing a line

--- js/main.js
+++ js/main.js
@@ -24,7 +24,8 @@ function createCanvas(id, parent, styles) {
     canvas.id = id;
 
     // Apply styles if needed
-    for (const style of styles) {
+    for (const style of styles) 
+    {
         canvas.classList.add(style);
     }
     canvas.width = 400;
@@ -43,5 +44,25 @@ function createCanvas(id, parent, styles) {
 
     return canvas;
 }
+
+function drawLine(canvas, p1, p2)
+{
+    //finding the canvas
+    
+    const ctx = canvas.getContext("2d");
+    
+    //styling the brush
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "black";
+    
+    //drawing the line
+    ctx.beginPath();
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(p2.x, p2.y);
+    ctx.stroke();
+}
+
 canvas_wrapper_div = createDiv("canvas-wrapper-div", body_div, ["canvas_wrapper_div"])
 canvas = createCanvas("canvas", canvas_wrapper_div, []);
+
+drawLine(canvas=canvas, p1={x: 4, y: 8}, p2={x: 200, y: 200})
